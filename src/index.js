aa2f23764fbc8c9b6f25529efb13fb9a58ea7628
Subha Arun <subhashini258@gmail.com>
2020-08-11 18:20:29 -0400

    deploy my react app to github

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,9 +7,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App></App>,
   document.getElementById('root')
 );
 
