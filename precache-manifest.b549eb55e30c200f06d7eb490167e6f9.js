166c8e8fb36b7e4b784a13b9eb6b8571700ae4fe
Subha Arun <subhashini258@gmail.com>
2020-08-11 18:21:22 -0400

    Updates

---
+++ precache-manifest.b549eb55e30c200f06d7eb490167e6f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d408b65a7fd29a21d34a7d9ab5ffaba",
+    "url": "/ReactApp-EmployeeDetails/index.html"
+  },
+  {
+    "revision": "c049e2c54208ef41c5ea",
+    "url": "/ReactApp-EmployeeDetails/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "adc952781cf43b7d7189",
+    "url": "/ReactApp-EmployeeDetails/static/css/main.bae00331.chunk.css"
+  },
+  {
+    "revision": "c049e2c54208ef41c5ea",
+    "url": "/ReactApp-EmployeeDetails/static/js/2.94f5446b.chunk.js"
+  },
+  {
+    "revision": "d11ccfb27105d37b4cc024497745de0e",
+    "url": "/ReactApp-EmployeeDetails/static/js/2.94f5446b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adc952781cf43b7d7189",
+    "url": "/ReactApp-EmployeeDetails/static/js/main.79158f79.chunk.js"
+  },
+  {
+    "revision": "35d4bdd74f0fa32967bb",
+    "url": "/ReactApp-EmployeeDetails/static/js/runtime-main.fa161507.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactApp-EmployeeDetails/static/media/logo.5d5d9eef.svg"
+  }
+]);
